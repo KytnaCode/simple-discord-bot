0d77ee65d9fa4e445fdc1c7fa91f33300e7a1fa5
Kytnacode <kytnacode@gmail.com>
2024-03-30 10:09:47 -0700

    Change RequestVerifier's API
    Now there aren't checks in setters methods only `Verify` method can
    fail.

--- pkg/discord/verify.go
+++ pkg/discord/verify.go
@@ -2,7 +2,6 @@ package discord
 
 import (
 	"crypto/ed25519"
-	"fmt"
 	"slices"
 )
 
@@ -14,24 +13,12 @@ type RequestVerifier struct {
 	publicKey, signature, timestamp, bodyContent []byte
 }
 
-func (rv *RequestVerifier) SetPublicKey(pk []byte) error {
-	if len(pk) != ed25519.PublicKeySize {
-		return fmt.Errorf("public key bad size: expected %v got %v", ed25519.PublicKeySize, len(pk))
-	}
-
+func (rv *RequestVerifier) SetPublicKey(pk []byte) {
 	rv.publicKey = pk
-
-	return nil
 }
 
-func (rv *RequestVerifier) SetSignature(sg []byte) error {
-	if len(sg) != ed25519.SignatureSize {
-		return fmt.Errorf("signature bad size: expected %v got %v", ed25519.SignatureSize, len(sg))
-	}
-
+func (rv *RequestVerifier) SetSignature(sg []byte) {
 	rv.signature = sg
-
-	return nil
 }
 
 func (rv *RequestVerifier) SetTimestamp(ts []byte) {
--- pkg/discord/verify_test.go
+++ pkg/discord/verify_test.go
@@ -3,74 +3,11 @@ package discord_test
 import (
 	"crypto/ed25519"
 	"crypto/rand"
-	"encoding/hex"
 	"slices"
 	"testing"
 
 	"github.com/kytnacode/simple-discord-bot/pkg/discord"
 )
-
-func TestRequestVerifier_SetPublicKeyShouldAcceptValidPublicKey(t *testing.T) {
-	t.Parallel()
-
-	pk, _, err := ed25519.GenerateKey(rand.Reader)
-	if err != nil {
-		t.Errorf("error generating keys: %v", err)
-	}
-
-	rv := new(discord.RequestVerifier)
-	err = rv.SetPublicKey(pk)
-
-	if err != nil {
-		t.Errorf("error on `RequestVerifier.SetPublicKey`: %v", err)
-	}
-}
-
-func TestRequestVerifier_SetPublicKeyShouldReturnAnErrorWithWrongSizedkey(t *testing.T) {
-	t.Parallel()
-
-	pk := []byte("my invalid-length key")
-
-	rv := new(discord.RequestVerifier)
-	err := rv.SetPublicKey(pk)
-
-	if err == nil {
-		t.Error("SetPublicKey must return an error with a wrong-sized key")
-	}
-}
-
-func TestRequestVerifier_SetSignatureShouldAcceptValidSignature(t *testing.T) {
-	t.Parallel()
-
-	msg := []byte("Hello World!")
-
-	_, prk, err := ed25519.GenerateKey(rand.Reader)
-	if err != nil {
-		t.Errorf("error on key generating: %v", err)
-	}
-
-	sg := ed25519.Sign(prk, msg)
-
-	rv := new(discord.RequestVerifier)
-
-	err = rv.SetSignature(sg)
-	if err != nil {
-		t.Errorf("SetSignature returned an error with a valid signature: %v", err)
-	}
-}
-
-func TestRequestVerifier_SetSignatureShouldReturnAnErrorWithWrongSizedSignature(t *testing.T) {
-	t.Parallel()
-
-	sg := []byte("my invalid signature")
-
-	rv := new(discord.RequestVerifier)
-
-	err := rv.SetSignature(sg)
-	if err == nil {
-		t.Error("SetSignature returned a nil error with an invalid signature")
-	}
-}
 
 func TestRequestVerifier_VerifyShouldReturnTrueWithValidSignatureKeyAndMessage(t *testing.T) {
 	t.Parallel()
@@ -87,19 +24,8 @@ func TestRequestVerifier_VerifyShouldReturnTrueWithValidSignatureKeyAndMessage(t
 
 	rv := new(discord.RequestVerifier)
 
-	err = rv.SetPublicKey(pk)
-	if err != nil {
-		t.Errorf("SetPublicKey return an error with a valid key: key %v", hex.EncodeToString(pk))
-	}
-
-	err = rv.SetSignature(sg)
-	if err != nil {
-		t.Errorf(
-			"SetSignature reuturn  an error with a valid signature: signature %v",
-			hex.EncodeToString(sg),
-		)
-	}
-
+	rv.SetPublicKey(pk)
+	rv.SetSignature(sg)
 	rv.SetTimestamp(ts)
 	rv.SetBodyContent(bc)
 
@@ -131,16 +57,8 @@ func TestRequestVerifier_VerifyShouldReturnFalseWithInvalidInputs(t *testing.T) 
 
 	rv := new(discord.RequestVerifier)
 
-	err = rv.SetPublicKey(pk)
-	if err != nil {
-		t.Errorf("invalid public key: %v", err)
-	}
-
-	err = rv.SetSignature(sg)
-	if err != nil {
-		t.Errorf("invalid public key: %v", err)
-	}
-
+	rv.SetPublicKey(pk)
+	rv.SetSignature(sg)
 	rv.SetTimestamp(ts)
 	rv.SetBodyContent(bc)
 
